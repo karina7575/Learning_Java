b46b239a2e08a8ff689cc7cbfd304530e77581a1
karina7575 <karinakios@mail.ru>
2023-01-23 23:58:15 +0300

    23.01.23

---
+++ RandomInt.java
@@ -0,0 +1,11 @@
+public class RandomInt
+{
+    public static void main (String[] args)
+    {
+        double f = -87;
+        double sinus = Math.sin (f);
+        double cosinus = Math.cos (f);
+
+        System.out.println(sinus * sinus + cosinus * cosinus);
+    }
+}
---
+++ excercise.java
@@ -0,0 +1,16 @@
+public class excercise
+{
+        public static void main (String[] args)
+        {
+            double f = -87;
+            double sinus = Math.sin (f);
+            double cosinus = Math.cos (f);
+
+            System.out.println(sinus * sinus + cosinus * cosinus);
+            System.out.println((2 + 3) + "bc");
+            System.out.println("bc" + 2 + 3);
+            System.out.println('b' + 'c');
+            System.out.println((char) ('a' + 4));
+
+        }
+}
