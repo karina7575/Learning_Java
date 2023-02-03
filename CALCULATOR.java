f29c660ea10a73b30f2168fc03fc8a4ea2524395
karina7575 <karinakios@mail.ru>
2023-02-03 23:59:50 +0300

    02.02.23

---
+++ CALCULATOR.java
@@ -0,0 +1,9 @@
+public class CALCULATOR
+{
+    public static void main(String [] args)
+    {
+        int a = Integer.parseInt(args[0]);
+        int sign = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[3]);
+    }
+}
